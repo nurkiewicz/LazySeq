441ca851a11d86a2b34e5294749ab30d5657b443
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2013-05-11 19:48:59 +0200

    Splitting and renaming tests

--- src/test/java/com/blogspot/nurkiewicz/lazyseq/LazySeqSliceTest.java
+++ src/test/java/com/blogspot/nurkiewicz/lazyseq/LazySeqSliceTest.java
@@ -1,116 +1,18 @@
 package com.blogspot.nurkiewicz.lazyseq;
 
-import org.mockito.Mock;
 import org.testng.annotations.Test;
 
-import java.util.function.Supplier;
-
-import static com.blogspot.nurkiewicz.lazyseq.LazySeq.*;
+import static com.blogspot.nurkiewicz.lazyseq.LazySeq.empty;
+import static com.blogspot.nurkiewicz.lazyseq.LazySeq.of;
 import static com.blogspot.nurkiewicz.lazyseq.SampleStreams.naturals;
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.fest.assertions.api.Assertions.failBecauseExceptionWasNotThrown;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 /**
  * @author Tomasz Nurkiewicz
- * @since 5/11/13, 6:07 PM
+ * @since 5/11/13, 7:36 PM
  */
-public class LazySeqSubstreamTest extends AbstractBaseTestCase {
-
-	@Mock
-	private Supplier<LazySeq<Integer>> supplierMock;
-
-	@Test
-	public void shouldThrowWhenNegativeStartIndexOnEmptySeq() throws Exception {
-		//given
-		final LazySeq<Object> empty = empty();
-
-		try {
-			//when
-			long startInclusive = -1;
-			empty.drop(startInclusive);
-			failBecauseExceptionWasNotThrown(IllegalArgumentException.class);
-		} catch (IllegalArgumentException e) {
-			//then
-		}
-	}
-
-	@Test
-	public void shouldThrowWhenNegativeStartIndexOnNonEmptySeq() throws Exception {
-		//given
-		final LazySeq<Integer> nonEmpty = of(1, 2, 3);
-
-		try {
-			//when
-			long startInclusive = -1;
-			nonEmpty.drop(startInclusive);
-			failBecauseExceptionWasNotThrown(IllegalArgumentException.class);
-		} catch (IllegalArgumentException e) {
-			//then
-		}
-	}
-
-	@Test
-	public void shouldThrowWhenNegativeStartIndexOnInfiniteSeq() throws Exception {
-		//given
-		final LazySeq<Integer> infinite = SampleStreams.primes();
-
-		try {
-			//when
-			long startInclusive = -1;
-			infinite.drop(startInclusive);
-			failBecauseExceptionWasNotThrown(IllegalArgumentException.class);
-		} catch (IllegalArgumentException e) {
-			//then
-		}
-	}
-
-	@Test
-	public void shouldReturnEmptySeqWhenSubstreamFromBeginningOfEmptySeq() throws Exception {
-		assertThat(empty().drop((long) 0)).isEmpty();
-	}
-
-	@Test
-	public void shouldReturnEmptySeqWhenSubstreamFromFurtherIndexOfEmptySeq() throws Exception {
-		assertThat(empty().drop((long) 5)).isEmpty();
-	}
-
-	@Test
-	public void shouldReturnEmptySeqWhenSubstreamPastEnd() throws Exception {
-		assertThat(of(1, 2).drop((long) 2)).isEmpty();
-	}
-
-	@Test
-	public void shouldReturnSubstreamWithoutPrefix() throws Exception {
-		assertThat(of(1, 2, 3, 4, 5).drop((long) 2)).isEqualTo(of(3, 4, 5));
-	}
-
-	@Test
-	public void shouldReturnSubstreamOfInfiniteStream() throws Exception {
-		//given
-		final LazySeq<Integer> naturals = naturals(0);
-
-		//when
-		final LazySeq<Integer> fromFive = naturals.drop((long) 5);
-
-		//then
-		assertThat(fromFive.take(4)).isEqualTo(of(5, 6, 7, 8));
-	}
-
-	@Test
-	public void shouldNotEvaluateTailExceptFirstElement() throws Exception {
-		//given
-		final LazySeq<Integer> naturals = LazySeq.of(1, supplierMock);
-		given(supplierMock.get()).willReturn(cons(2, (Supplier<LazySeq<Integer>>) supplierMock::get));
-
-		//when
-		naturals.drop((long) 2);
-
-		//then
-		verify(supplierMock, times(2)).get();
-	}
+public class LazySeqSliceTest extends AbstractBaseTestCase {
 
 	@Test
 	public void shouldReturnEmptySeqWhenSlicingEmptySeq() throws Exception {
