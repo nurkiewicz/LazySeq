9e18252e8093977205fa7098e268d4b969c42d9e
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2013-05-12 17:10:20 +0200

    Samples of lazily loading database records

---
+++ src/test/java/com/blogspot/nurkiewicz/lazyseq/samples/Record.java
@@ -0,0 +1,22 @@
+package com.blogspot.nurkiewicz.lazyseq.samples;
+
+class Record {
+	private final int id;
+
+	Record(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Record)) return false;
+		Record record = (Record) o;
+		return id == record.id;
+	}
+
+	@Override
+	public int hashCode() {
+		return id;
+	}
+}
